b0a6241b6d045a0fcfe0e32777c6a5a915142b60
Thyago Mota <thyagomota@gmail.com>
2022-06-12 14:34:43 -0600

    updates

---
+++ activity_08_polynomial/src/Polynomial.java
@@ -0,0 +1,50 @@
+/*
+ * CS 2050 - Computer Science II - Summer 2022
+ * Instructor: Thyago Mota
+ * Description: Activity 08 - Polynomial
+ */
+
+public class Polynomial {
+
+    private Term firstTerm;
+
+    public Polynomial() {
+        firstTerm = null;
+    }
+
+    // TODO: return the maximum exponent value of the polynomial
+    public int degree() {
+
+    }
+
+    // TODO: return the number of terms of the polynomial
+    public int size() {
+        return 0;
+    }
+
+    // TODO: add the given term to the polynomial (see activity instructions for more detailed information)
+    void addTerm(final Term term) {
+
+    }
+
+    // TODO: add the terms of the given polynomial to the callee polynomial; hint: traverse the given
+    //  polynomial and call addTerm to add each of its terms to the callee polynomial
+    void add(final Polynomial other) {
+
+    }
+
+    // TODO: traverse the callee polynomial and call toString from each of its terms to generate
+    //  a string representation of the polynomial; use the following examples to figure it out the
+    //  format to use:
+    //  8x^3 -2x^2 + 7x + 3.2
+    @Override
+    public String toString() {
+        return "";
+    }
+
+    // TODO: return the term with the given exponent; null if the term does not exist
+    Term getTerm(int exponent) {
+
+        return null;
+    }
+}
---
+++ activity_08_polynomial/src/Term.java
@@ -0,0 +1,76 @@
+/*
+ * CS 2050 - Computer Science II - Summer 2022
+ * Instructor: Thyago Mota
+ * Description: Activity 08 - Term
+ */
+
+public class Term {
+
+    private double coefficient;
+    private int    exponent;
+    private Term   next;
+
+    private static final double TOLERANCE = 0.00000001;
+    private static final double DEFAULT_COEFFICIENT = 0;
+
+    public Term(double coefficient, int exponent) {
+        if (Math.abs(coefficient) <= TOLERANCE)
+            this.coefficient = DEFAULT_COEFFICIENT;
+        else
+            this.coefficient = coefficient;
+        this.exponent = exponent;
+        this.next = null;
+    }
+
+    public Term(double coefficient) {
+        this(coefficient, 0);
+    }
+
+    public Term() {
+        this(1, 0);
+    }
+
+    public double getCoefficient() {
+        return coefficient;
+    }
+
+    public int getExponent() {
+        return exponent;
+    }
+
+    public Term getNext() {
+        return next;
+    }
+
+    public void setNext(final Term next) {
+        this.next = next;
+    }
+
+    // TODO: add the given term's coefficient to the callee term's coefficient if and only if they have the same exponent value
+    public void add(final Term other) {
+
+    }
+
+    // TODO: two terms are considered to be equal if they have the same values for their coefficient and exponent; note that coefficient is a
+    //  floating-point value, so make sure you accept a TOLERANCE when comparing two coefficients for equality
+    @Override
+    public boolean equals(Object obj) {
+        return false;
+    }
+
+    // TODO: use the following examples to figure it out the format to use:
+    //  8x^3
+    //  -2x^2
+    //  7x
+    //  3.2
+    @Override
+    public String toString() {
+
+    }
+
+    // TODO: return a new term that has the same coefficient and exponent of the callee term
+    @Override
+    public Object clone()  {
+        return null;
+    }
+}
