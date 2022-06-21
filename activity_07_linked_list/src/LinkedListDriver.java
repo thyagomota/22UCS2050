b22558820fdee11f4d917cc0a8fb90a1e291d13f
Thyago Mota <thyagomota@gmail.com>
2022-06-20 18:01:52 -0600

    updates

--- activity_07_linked_list/src/LinkedListDriver.java
+++ activity_07_linked_list/src/LinkedListDriver.java
@@ -5,4 +5,20 @@
  */
 
 public class LinkedListDriver {
+
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        System.out.println(ll);
+        ll.add(3);
+        System.out.println(ll);
+        ll.add(5);
+        System.out.println(ll);
+        ll.add(8);
+        System.out.println(ll);
+        System.out.println(ll.get(0));
+        System.out.println(ll.get(1));
+        System.out.println(ll.get(2));
+        System.out.println(ll.get(3));
+
+    }
 }
