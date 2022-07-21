5dc3a4c9dff809c68594e8461a13182c10375f90
Thyago Mota <thyagomota@gmail.com>
2022-07-21 14:56:56 -0600

    updates

--- activity_21_sorting_objects/src/SortingObjects.java
+++ activity_21_sorting_objects/src/SortingObjects.java
@@ -5,7 +5,8 @@
  */
 
 import java.io.*;
-import java.util.*;
+import java.util.Iterator;
+import java.util.Scanner;
 
 public class SortingObjects {
 
@@ -13,24 +14,55 @@ public class SortingObjects {
 
     // TODO: merge the two given lists returning a new list with the elements sorted
     public static LinkedList<Student> merge(LinkedList<Student> left, LinkedList<Student> right) {
-        return null;
+        LinkedList<Student> sorted = new LinkedList<>();
+        Iterator<Student> itLeft = left.iterator();
+        Student leftStudent = itLeft.next();
+        Iterator<Student> itRight = right.iterator();
+        Student rightStudent = itRight.next();
+        while (leftStudent != null && rightStudent != null) {
+            if (leftStudent.compareTo(rightStudent) < 0) {
+                sorted.append(leftStudent);
+                leftStudent = itLeft.next();
+            }
+            else {
+                sorted.append(rightStudent);
+                rightStudent = itRight.next();
+            }
+        }
+        while (leftStudent != null) {
+            sorted.append(leftStudent);
+            leftStudent = itLeft.next();
+        }
+        while (rightStudent != null) {
+            sorted.append(rightStudent);
+            rightStudent = itRight.next();
+        }
+        return sorted;
     }
 
     // TODO: implement merge sort using a linked list
     public static LinkedList<Student> mergeSort(LinkedList<Student> students) {
 
         // TODO: implement the base case
-
+        if (students.size() <= 1)
+            return students;
 
         // TODO: divide the students list into two lists: left and right
-
-
+        LinkedList<Student> left = new LinkedList<>();
+        LinkedList<Student> right = new LinkedList<>();
+        int middle = students.size() / 2;
+        Iterator<Student> it = students.iterator();
+        for (int i = 0; i < middle; i++)
+            left.append(it.next());
+        while (it.hasNext())
+            right.append(it.next());
 
         // TODO: recursively call mergeSort w/ left and right linked lists
-
+        left = mergeSort(left);
+        right = mergeSort(right);
 
         // TODO: return the result of merging left and right linked lists
-        return null;
+        return merge(left, right);
     }
 
     public static void main(String[] args) throws FileNotFoundException {
