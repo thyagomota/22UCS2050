ab84891a9aed01db57b70473f2949f3d8525f8fc
Thyago Mota <thyagomota@gmail.com>
2022-07-28 17:16:36 -0600

    updates

--- activity_23_expr_trees/src/ExpressionTree.java
+++ activity_23_expr_trees/src/ExpressionTree.java
@@ -8,8 +8,10 @@ public class ExpressionTree {
 
     private BinNode<Term> root;
 
-    public ExpressionTree(Queue<Term> input) {
+    public ExpressionTree(Queue<Term> input) throws InstantiationException{
         root = addRecursively(input);
+        if (!input.isEmpty())
+            throw new InstantiationException("Term(s) still remain to be processed!");
     }
 
     public boolean isEmpty() {
@@ -17,22 +19,35 @@ public class ExpressionTree {
     }
 
     // TODO: implement the addRecursively private method
-    private BinNode<Term> addRecursively(Queue<Term> input) {
+    private BinNode<Term> addRecursively(Queue<Term> input) throws InstantiationException {
 
         // TODO: if the input queue is empty, return right away (nothing to do)
+        if (input.isEmpty())
+            return null;
 
-        // TODO: consume one term from the input queue (e.g., pop the input queue saving the term reference)
+        // TODO: consume one term from the input queue (e.g., pop the input queue saving the term 's reference)
+        Term term = input.pop();
 
         // TODO: create a new binary node with the term
+        BinNode<Term> newNode = new BinNode<>(term);
 
         // TODO: get current node's term type
+        int type = term.getType();
 
-        // TODO: if type is operator, make recursive calls to the left and to the right, updating the left and
-        //  right references of the newly created node with the returned values
-
+        // TODO: if type is operator, make recursive calls to the left and to the right, updating the left and right references of the newly created node with the returned values
+        if (type == Term.OPERATOR_TYPE) {
+            BinNode<Term> leftNode = addRecursively(input);
+            if (leftNode == null)
+                throw new InstantiationException("Term expected on the left of " + term);
+            newNode.setLeft(leftNode);
+            BinNode<Term> rightNode = addRecursively(input);
+            if (rightNode == null)
+                throw new InstantiationException("Term expected on the right of " + term);
+            newNode.setRight(rightNode);
+        }
 
         // TODO: don't forget to return the new node
-        return null;
+        return newNode;
     }
 
     public String inOrder(final BinNode<Term> current) {
--- activity_24_hashtable/src/Hashtable.java
+++ activity_24_hashtable/src/Hashtable.java
@@ -6,4 +6,27 @@
 
 public class Hashtable<K,V> {
 
+    private final int SIZE = 11;
+    private HashNode<K,V>[] table;
+
+    public Hashtable() {
+        table = new HashNode[SIZE];
+    }
+
+    public void put(K key, V value) {
+        int index = key.hashCode() % 11;
+        HashNode<K, V> newHashNode = new HashNode<>(key, value);
+        HashNode<K, V> current = table[index];
+        if (current == null)
+            table[index] = newHashNode;
+        else {
+            while (current.getNext() != null)
+                current = current.getNext();
+            current.setNext(newHashNode);
+        }
+    }
+
+    public V get(K key) {
+        return null;
+    }
 }
